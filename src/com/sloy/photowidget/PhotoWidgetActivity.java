137dec549eced417e04feb21c19b334fe594303f
Sloy <rafa.vazsan@gmail.com>
2012-02-17 16:43:01 +0100

    Interfaz basada en pestañas superiores.
    * Creadas 2 pestañas: configuración de fuentes y de widgets
    * Implementado cambio entre pestañas: cambia el fragment de la lista

--- src/com/sloy/photowidget/PhotoWidgetActivity.java
+++ src/com/sloy/photowidget/PhotoWidgetActivity.java
@@ -1,27 +1,128 @@
 package com.sloy.photowidget;
 
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
-import android.app.ListFragment;
-import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.util.TypedValue;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.ScrollView;
-import android.widget.TextView;
 
 public class PhotoWidgetActivity extends Activity {
+
+	public static enum TabType {
+		FUENTES(FuentesListFragment.class.getName()), WIDGETS(WidgetListFragment.class.getName());
+
+		private String mName;
+
+		TabType(String classname) {
+			mName = classname;
+		}
+
+		public String getClassName() {
+			return mName;
+		}
+	}
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		final ActionBar bar = getActionBar();
+		bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		bar.setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);
+		bar.addTab(bar.newTab().setText("Fuentes").setTabListener(new TabListener(this, "fuentes", TabType.FUENTES)));
+		bar.addTab(bar.newTab().setText("Widgets").setTabListener(new TabListener(this, "widgets", TabType.WIDGETS)));
+	}
+
+	public class TabListener implements ActionBar.TabListener {
+		private Fragment mFragment;
+		private Activity mActivity;
+		private TabType mTabType;
+		private final String mTag;
+		private final TabType mClass;
+		private final Bundle mArgs;
+
+		public TabListener(Activity activity, String tag, TabType clz) {
+			this(activity, tag, clz, null);
+		}
+
+		public TabListener(Activity activity, String tag, TabType clz, Bundle args) {
+			mActivity = activity;
+			mTag = tag;
+			mClass = clz;
+			mArgs = args;
+
+			// Check to see if we already have a fragment for this tab, probably
+			// from a previously saved state. If so, deactivate it, because our
+			// initial state is that a tab isn't shown.
+			mFragment = mActivity.getFragmentManager().findFragmentByTag(mTag);
+			if(mFragment != null && !mFragment.isDetached()){
+				FragmentTransaction ft = mActivity.getFragmentManager().beginTransaction();
+				ft.detach(mFragment);
+				ft.commit();
+			}
+		}
+
+		@Override
+		public void onTabSelected(Tab tab, FragmentTransaction ft) {
+			if(mFragment == null){
+				mFragment = Fragment.instantiate(mActivity, mClass.getClassName(), mArgs);
+				ft.add(R.id.titles, mFragment, mTag);
+			}else{
+				ft.attach(mFragment);
+			}
+			//
+			/*
+			 * Fragment present =
+			 * getFragmentManager().findFragmentById(R.id.titles);
+			 * if(mFragment == null){
+			 * switch (mTabType){
+			 * case FUENTES:
+			 * mFragment = Fragment.instantiate(mActivity,
+			 * FuentesListFragment.class.getName());
+			 * break;
+			 * case WIDGETS:
+			 * mFragment = Fragment.instantiate(mActivity,
+			 * WidgetListFragment.class.getName());
+			 * break;
+			 * }
+			 * }
+			 * ft.remove(present);
+			 * ft.replace(R.id.titles, mFragment);
+			 * Fragment fragment = null;
+			 * switch (mTabType){
+			 * case FUENTES:
+			 * if(!(present instanceof FuentesListFragment)){
+			 * fragment = Fragment.instantiate(mActivity,
+			 * FuentesListFragment.class.getName());
+			 * }
+			 * break;
+			 * case WIDGETS:
+			 * if(!(present instanceof WidgetListFragment)){
+			 * fragment = Fragment.instantiate(mActivity,
+			 * WidgetListFragment.class.getName());
+			 * }
+			 * break;
+			 * }
+			 * if(fragment != null){
+			 * }
+			 */
+
+		}
+
+		@Override
+		public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		}
+
+		@Override
+		public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+			if(mFragment != null){
+				ft.detach(mFragment);
+			}
+		}
+
 	}
 
 	public static class DetailsActivity extends Activity {
@@ -39,133 +140,10 @@ public class PhotoWidgetActivity extends Activity {
 
 			if(savedInstanceState == null){
 				// During initial setup, plug in the details fragment.
-				DetailsFragment details = new DetailsFragment();
+				ConfigCarpetaFragment details = new ConfigCarpetaFragment();
 				details.setArguments(getIntent().getExtras());
 				getFragmentManager().beginTransaction().add(android.R.id.content, details).commit();
 			}
-		}
-	}
-
-	public static class TitlesFragment extends ListFragment {
-		boolean mDualPane;
-		int mCurCheckPosition = 0;
-
-		@Override
-		public void onActivityCreated(Bundle savedInstanceState) {
-			super.onActivityCreated(savedInstanceState);
-
-			// Populate list with our static array of titles.
-			setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_activated_1, Shakespeare.TITLES));
-
-			// Check to see if we have a frame in which to embed the details
-			// fragment directly in the containing UI.
-			View detailsFrame = getActivity().findViewById(R.id.details);
-			mDualPane = detailsFrame != null && detailsFrame.getVisibility() == View.VISIBLE;
-
-			if(savedInstanceState != null){
-				// Restore last state for checked position.
-				mCurCheckPosition = savedInstanceState.getInt("curChoice", 0);
-			}
-
-			if(mDualPane){
-				// In dual-pane mode, the list view highlights the selected
-				// item.
-				getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-				// Make sure our UI is in the correct state.
-//				showDetails(mCurCheckPosition);
-			}
-		}
-
-		@Override
-		public void onSaveInstanceState(Bundle outState) {
-			super.onSaveInstanceState(outState);
-			outState.putInt("curChoice", mCurCheckPosition);
-		}
-
-		@Override
-		public void onListItemClick(ListView l, View v, int position, long id) {
-			showDetails(position);
-		}
-
-		/**
-		 * Helper function to show the details of a selected item, either by
-		 * displaying a fragment in-place in the current UI, or starting a
-		 * whole new activity in which it is displayed.
-		 */
-		void showDetails(int index) {
-			mCurCheckPosition = index;
-
-			if(mDualPane){
-				// We can display everything in-place with fragments, so update
-				// the list to highlight the selected item and show the data.
-				getListView().setItemChecked(index, true);
-
-				// Check what fragment is currently shown, replace if needed.
-				DetailsFragment details = (DetailsFragment)getFragmentManager().findFragmentById(R.id.details);
-				if(details == null || details.getShownIndex() != index){
-					// Make new fragment to show this selection.
-					details = DetailsFragment.newInstance(index);
-
-					// Execute a transaction, replacing any existing fragment
-					// with this one inside the frame.
-					FragmentTransaction ft = getFragmentManager().beginTransaction();
-					ft.replace(R.id.details, details);
-					ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-					ft.commit();
-				}
-
-			}else{
-				// Otherwise we need to launch a new activity to display
-				// the dialog fragment with selected text.
-				Intent intent = new Intent();
-				intent.setClass(getActivity(), DetailsActivity.class);
-				intent.putExtra("index", index);
-				startActivity(intent);
-			}
-		}
-
-	}
-
-	public static class DetailsFragment extends Fragment {
-		/**
-		 * Create a new instance of DetailsFragment, initialized to
-		 * show the text at 'index'.
-		 */
-		public static DetailsFragment newInstance(int index) {
-			DetailsFragment f = new DetailsFragment();
-
-			// Supply index input as an argument.
-			Bundle args = new Bundle();
-			args.putInt("index", index);
-			f.setArguments(args);
-
-			return f;
-		}
-
-		public int getShownIndex() {
-			return getArguments().getInt("index", 0);
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-			if(container == null){
-				// We have different layouts, and in one of them this
-				// fragment's containing frame doesn't exist. The fragment
-				// may still be created from its saved state, but there is
-				// no reason to try to create its view hierarchy because it
-				// won't be displayed. Note this is not needed -- we could
-				// just run the code below, where we would create and return
-				// the view hierarchy; it would just never be used.
-				return null;
-			}
-
-			ScrollView scroller = new ScrollView(getActivity());
-			TextView text = new TextView(getActivity());
-			int padding = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getActivity().getResources().getDisplayMetrics());
-			text.setPadding(padding, padding, padding, padding);
-			scroller.addView(text);
-			text.setText(Shakespeare.DIALOGUE[getShownIndex()]);
-			return scroller;
 		}
 	}
 
