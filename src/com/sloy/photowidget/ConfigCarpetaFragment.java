8d6434477767ee7c227f1805ea6fe078866ff110
Sloy <rafa.vazsan@gmail.com>
2012-02-18 14:13:14 +0100

    Guarda el nombre y dirección de las fuentes al pulsar guardar

--- src/com/sloy/photowidget/ConfigCarpetaFragment.java
+++ src/com/sloy/photowidget/ConfigCarpetaFragment.java
@@ -2,11 +2,14 @@ package com.sloy.photowidget;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.dataframework.DataFramework;
 import com.android.dataframework.Entity;
 
 public class ConfigCarpetaFragment extends Fragment {
@@ -34,12 +37,12 @@ public class ConfigCarpetaFragment extends Fragment {
 	public long getShownId() {
 		return getArguments().getLong("id", 0);
 	}
-	
-	public String getNombre(){
+
+	public String getNombre() {
 		return getArguments().getString("nombre");
 	}
-	
-	public String getDireccion(){
+
+	public String getDireccion() {
 		return getArguments().getString("direccion");
 	}
 
@@ -56,10 +59,34 @@ public class ConfigCarpetaFragment extends Fragment {
 			return null;
 		}
 
-		View v = inflater.inflate(R.layout.config_carpeta, container,false);
-		((EditText)v.findViewById(R.id.editText1)).setText(getNombre());
-		((EditText)v.findViewById(R.id.editText2)).setText(getDireccion());
+		View v = inflater.inflate(R.layout.config_carpeta, container, false);
+		final EditText nombre = ((EditText)v.findViewById(R.id.editText1));
+		nombre.setText(getNombre());
 		
+		final EditText direccion = ((EditText)v.findViewById(R.id.editText2));
+		direccion.setText(getDireccion());
+		
+		((Button)v.findViewById(R.id.button1)).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				DataFramework db = null;
+				try{
+					db = DataFramework.getInstance();
+					db.open(getActivity(), getActivity().getPackageName());
+					Entity e = new Entity("fuentes_carpeta", getShownId());
+					e.setValue("nombre", nombre.getText().toString());
+					e.setValue("direccion", direccion.getText().toString());
+					e.save();
+				}catch(Exception e){
+					Log.e("PhotoWidget", "Error guardando la configuración del widget", e);
+				}
+				if(db != null){
+					db.close();
+				}
+				//TODO actualizar todos los widgets que usen esta fuente
+			}
+		});
+
 		return v;
 	}
 }
